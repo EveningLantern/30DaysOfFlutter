47138b32bc126e79045c6f11e85996ca0a6ea9e3
Evening <sayandeep123saha@gmail.com>
2024-06-27 07:42:45 +0530

    day-5 completed we tried grid view which was not really good

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -43,11 +43,38 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(color: Color(0xff6C63FF)),
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        child: Image.network(item.image),
+                        footer: Text(item.price.toString()),
+                      ));
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            /*ListView.builder(
                 itemCount: CatalogModel.items.length,
                 itemBuilder: (context, index) =>
                     ItemWidget(item: CatalogModel.items[index]),
-              )
+              )*/
             : const Center(
                 child: CircularProgressIndicator(
                   color: Color(0xff6C63FF),
